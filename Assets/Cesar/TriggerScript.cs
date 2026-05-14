bd5dd4ab06d4e0a8f51061e882853f512a1d8c06
CesarNuno <cgnuno2001@gmail.com>
2026-05-13 18:59:14 -0600

    Tarea 1

--- Assets/Cesar/TriggerScript.cs
+++ Assets/Cesar/TriggerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class TriggerScript : MonoBehaviour
@@ -5,6 +6,7 @@ public class TriggerScript : MonoBehaviour
     private SpriteRenderer sprite;
     private Color ogColor;
     public Color changedColor;
+    private Boolean deleting = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,6 +25,48 @@ public class TriggerScript : MonoBehaviour
         if (collision.CompareTag("Trigger")) {
             Debug.Log("Enter");
             sprite.color = changedColor;
+        }
+        Debug.Log(collision.tag);
+        switch (collision.tag)
+        {
+            case "Color":
+                sprite.color = new Color(123, 456, 789);
+                break;
+
+            case "Size":
+                sprite.transform.localScale = new Vector3(2,2,2);
+                break;
+
+            case "Destroy":
+                break;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Color"))
+        {
+            Debug.Log("asdfasfd");
+            sprite.color = new Color(0.1f,0.2f,0.3f,1f);
+        }
+        else if (collision.gameObject.CompareTag("Size"))
+        {
+            sprite.transform.localScale = new Vector3(2, 2, 2);
+        }
+        else if (collision.gameObject.CompareTag("Destroy"))
+        {
+
+            if (!deleting)
+            {
+                deleting = true;
+                ParticleSystem part = gameObject.GetComponent<ParticleSystem>();
+                AudioSource sound = gameObject.GetComponent<AudioSource>();
+                if(!sound.isPlaying) sound.Play();
+                part.Play();
+                Destroy(gameObject, gameObject.GetComponent<ParticleSystem>().main.duration);
+                deleting = false;
+            }
+            
         }
     }
 
