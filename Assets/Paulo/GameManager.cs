c76b95f4edb63455b38e699a58737fd4def78893
Paulo <eltaquitosdorados@gmail.com>
2026-05-28 21:19:23 -0600

    Tutorial terminado

--- Assets/Paulo/GameManager.cs
+++ Assets/Paulo/GameManager.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,6 +10,24 @@ public class GameManager : MonoBehaviour
     public GameObject objetoPadrisimo;
     public float spawnerTime = 1;
     public bool startSpawn = false;
+
+    public static GameManager instance;
+
+    public int goldenPuzzlePieces = 0;
+    public TextMeshProUGUI contadorTxt;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } 
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -43,5 +63,12 @@ public class GameManager : MonoBehaviour
         var newObject = Instantiate(objetoPadrisimo);
 
         newObject.transform.position = objectTransform;
+    }
+
+    public void AddPuzzlePiece()
+    {
+        goldenPuzzlePieces++;
+        
+        contadorTxt.text = "x " + goldenPuzzlePieces;
     }
 }
